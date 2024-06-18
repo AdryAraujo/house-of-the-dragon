4cb09a6df404aa3c52381acadc6f0d4e5a744ce3
Adryelle Araujo <191036532940@academico.ufs.br>
2024-06-18 16:39:21 -0300

    finalizando Navbar Responsiva

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,37 @@
 import { useState } from 'react';
 import './Navbar.css';
+import { FaBars, FaTimes } from 'react-icons/fa'
+import logo from '../assets/logoTargaryen.png'
+
 
 function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false);
     return (
-        <div>
-            <nav className="navbar-image">
-                <div className='navbar-options'>
-                    {/* <button
-                        className="menu"
-                        onClick={() => setMenuOpen(!menuOpen)}>
-                        <FaBars className='menu-icon' />
-                    </button> */}
-                    <ul className={menuOpen ? "open" : ""}>
-                        <li>
-                            <a href="/">Início</a>
-                        </li>
-                        <li>
-                            <a href="/">Personagens</a>
-                        </li>
-                        <li>
-                            <a href="/">Resumo</a>
-                        </li>
-                        <li>
-                            <a href="/">Dragões</a>
-                        </li>
-                    </ul>
-                </div>
+        <div className="navbar-image">
+            <nav className='navbar-options'>
+                <img src={logo} alt="" />
 
+                <button
+                    className="menu"
+                    onClick={() => setMenuOpen(!menuOpen)}>
+                    <FaBars className='menu-icon' />
+                </button>
+                <ul className={menuOpen ? "open" : ""}>
+                    <li>
+                        <a href="/">Início</a>
+                    </li>
+                    <li>
+                        <a href="/">Personagens</a>
+                    </li>
+                    <li>
+                        <a href="/">Resumo</a>
+                    </li>
+                    <li>
+                        <a href="/">Dragões</a>
+                    </li>
+                </ul>
             </nav>
+
         </div>
     )
 }
