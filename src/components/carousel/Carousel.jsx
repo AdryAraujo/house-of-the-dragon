b13e7c7db2f8e99631e83f55e3a7f501edad3aa3
Adryelle Araujo <191036532940@academico.ufs.br>
2024-06-22 11:41:25 -0300

    consumindo dados de um json local

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,37 +1,28 @@
 import './Carousel.css'
 import daemon from '../../assets/daemon.jpg';
+import { useEffect, useState } from 'react';
 
 export function Carousel() {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() =>{
+        fetch('http://localhost:3000/static/data.json')
+        .then((response) => response.json())
+        .then(setData);
+    },[]);
+
     return (
         <div className='container-carousel'>
-            <div className="carousel-item">
-                <img src={daemon} alt="" />
-                <button></button>
-            </div>
-            <div className="carousel-item">
-                <img src={daemon} alt="" />
-                <button></button>
-            </div>
-            <div className="carousel-item">
-                <img src={daemon} alt="" />
-                <button></button>
-            </div>
-            <div className="carousel-item">
-                <img src={daemon} alt="" />
-                <button></button>
-            </div>
-            <div className="carousel-item">
-                <img src={daemon} alt="" />
-                <button></button>
-            </div>
-            <div className="carousel-item">
-                <img src={daemon} alt="" />
-                <button></button>
-            </div>
-            <div className="carousel-item">
-                <img src={daemon} alt="" />
-                <button></button>
-            </div>
+            {data.map((item) =>{
+                const {name, image} = item
+                return(
+                    <div className="carousel-item" key={name}>
+                    <img src={image} alt="" />
+                    {/* <button>{name}</button> */}
+                </div>
+                )
+            })}
         </div>
 
     )
