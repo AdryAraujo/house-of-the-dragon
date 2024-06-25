dafbe6b548c29afb27ccd2f3d0faf1ff9022d2ff
Adryelle Araujo <191036532940@academico.ufs.br>
2024-06-25 15:54:51 -0300

    carousel funcionando

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,29 +1,53 @@
 import './Carousel.css'
-import daemon from '../../assets/daemon.jpg';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 export function Carousel() {
 
     const [data, setData] = useState([]);
+    const carousel = useRef(null);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch('http://localhost:3000/static/data.json')
-        .then((response) => response.json())
-        .then(setData);
-    },[]);
+            .then((response) => response.json())
+            .then(setData);
+    }, []);
+
+    const handleLeftClick = (e) => {
+        e.preventDefault();
+        console.log(carousel.current.offsetWidth);
+        carousel.current.scrollLeft -= carousel.current.offsetWidth;
+    }
+    const handleRightClick = (e) => {
+        e.preventDefault();
+        console.log(carousel.current.offsetWidth);
+
+        carousel.current.scrollLeft += carousel.current.offsetWidth;
+    }
 
     return (
         <div className='container-carousel'>
-            {data.map((item) =>{
-                const {name, image} = item
-                return(
-                    <div className="carousel-item" key={name}>
-                    <img src={image} alt="" />
-                    {/* <button>{name}</button> */}
-                </div>
-                )
-            })}
+            <button 
+            className='carousel-button'
+            onClick={handleLeftClick}>
+                <FaChevronLeft style={{ width: "32px", height: "32px" }} />
+            </button>
+            <div className='content-carousel' ref={carousel}>
+                {data.map((item) => {
+                    const { name, image } = item
+                    return (
+                        <div className="carousel-item" key={name}>
+                            <img src={image} alt="" />
+                            {/* <button>{name}</button> */}
+                        </div>
+                    )
+                })}
+                
+            </div>
+            <button id='seta-right' className='carousel-button'
+            onClick={handleRightClick}>
+                <FaChevronRight  style={{ width: "32px", height: "32px" }} />
+            </button>
         </div>
-
     )
 }
